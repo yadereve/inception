b70997dde296c053754548950ea582c5ff7bb044
ubuntu <ubuntu@ubuntu.myguest.virtualbox.org>
2025-06-17 20:09:58 +0100

    script_test.sh:done

---
+++ srcs/requirements/wordpress/script.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+cd /var/www/html
+curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
+chmod +x wp-cli.phar
+
---
+++ srcs/requirements/wordpress/script_test.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+until mysqladmin ping -h"mariadb" --silent; do
+    echo "⏳ Waiting for MariaDB..."
+    sleep 2
+done
+
+cd /var/www/html
+curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
+chmod +x wp-cli.phar
+./wp-cli.phar core download --allow-root
+./wp-cli.phar config create \
+    --dbname=wordpress \
+    --dbuser=wpuser \
+    --dbpass=password \
+    --dbhost=mariadb \
+    --allow-root
+./wp-cli.phar core install \
+    --url=localhost \
+    --title=inception \
+    --admin_user=admin \
+    --admin_password=admin \
+    --admin_email=admin@admin.com \
+    --allow-root
+
+php-fpm7.4 -F
+
