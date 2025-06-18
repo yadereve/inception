af05439a87f93ee9bcb01674dd600b730cbceaab
ubuntu <ubuntu@ubuntu.myguest.virtualbox.org>
2025-06-18 16:31:30 +0100

    wordpress installed: ok

--- srcs/requirements/wordpress/script.sh
+++ srcs/requirements/wordpress/script.sh
@@ -1,6 +1,29 @@
 #!/bin/bash
 
+until mysqladmin ping -h"mariadb" --silent; do
+    echo "⏳ Waiting for MariaDB..."
+    sleep 2
+done
+
 cd /var/www/html
-curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
-chmod +x wp-cli.phar
+    curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
+    chmod +x wp-cli.phar
+if [ ! -f /var/www/http/wp-config.php ]; then
+    ./wp-cli.phar core download --allow-root
+    ./wp-cli.phar config create \
+        --dbname=wordpress \
+        --dbuser=wpuser \
+        --dbpass=password \
+        --dbhost=mariadb \
+        --allow-root
+    ./wp-cli.phar core install \
+        --url=localhost \
+        --title=inception \
+        --admin_user=admin \
+        --admin_password=admin \
+        --admin_email=admin@admin.com \
+        --allow-root
+fi
+
+php-fpm7.4 -F
 
